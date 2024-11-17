f161fd2ad2d728e77bdbaf153eb346b803446585
Ekaterina363 <batsl363@gmail.com>
2024-11-17 17:57:57 +0400

    Add files via upload

---
+++ module_1_6.py
@@ -0,0 +1,20 @@
+my_dict = { "r": 123, "d": 456, "h": 789 }
+print(my_dict)
+print(type(my_dict))
+print(my_dict["r"])
+print(my_dict.get("k"))
+my_dict.update ({"o": 639, "s": 852})
+del my_dict ["d"]
+print(my_dict)
+
+my_set = {1, 2, 3, 4, 5, 6, 7, 1, 2, 3, 4}
+print(my_set)
+print(type(my_set))
+print(my_set.add(8,))
+print(my_set)
+print(my_set.add(9,))
+print(my_set)
+print(my_set.remove(3))
+print(my_set)
+
+
